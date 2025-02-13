f347f04b8da9370e2c20f3c56e03b70ade0c776e
James Zian Xu <jameszianxu@tenstorrent.com>
2025-02-13 16:48:50 -0500

    Improve Pytest Parser (#21)
    - Add parsers for deserialization of python dict as
    	serialized by junitxml to capture config and tags
    - Add test coverage for tt_torch pytest full model tests

---
+++ .github/actions/collect_data/test/test_tt_torch_full_model_tests_parser.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+from parsers.python_pytest_parser import PythonPytestParser
+
+
+@pytest.mark.parametrize(
+    "tar, project, github_job_id, expected",
+    [
+        ("mnist.xml", "tt-torch", 5, {"tests_cnt": 2}),
+    ],
+)
+def test_tt_torch_full_model_tests_parser(tar, project, github_job_id, expected):
+    filepath = f"test/data/tt_torch_models/{tar}"
+    parser = PythonPytestParser()
+    assert parser.can_parse(filepath)
+    tests = parser.parse(filepath, project=project, github_job_id=github_job_id)
+    assert len(tests) == expected["tests_cnt"]
