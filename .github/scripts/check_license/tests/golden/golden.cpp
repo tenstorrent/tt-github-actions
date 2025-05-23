a27c146ddffb013790609087c4063185618fa83b
jmcgrathTT <jmcgrath@tenstorrent.com>
2025-05-23 08:08:58 +0000

    add

---
+++ .github/scripts/check_license/tests/golden/golden.cpp
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World" << std::endl;
+  return 0;
+}
