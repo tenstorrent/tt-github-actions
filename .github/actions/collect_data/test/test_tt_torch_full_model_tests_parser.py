7cbad59a9957a93568982b231cce9bea83eac5fa
James Zian Xu <jameszianxu@tenstorrent.com>
2025-09-09 21:23:58 +0000

    Add tests

--- .github/actions/collect_data/test/test_tt_torch_full_model_tests_parser.py
+++ .github/actions/collect_data/test/test_tt_torch_full_model_tests_parser.py
@@ -18,3 +18,20 @@ def test_tt_torch_full_model_tests_parser(tar, project, github_job_id, expected)
     assert parser.can_parse(filepath)
     tests = parser.parse(filepath, project=project, github_job_id=github_job_id)
     assert len(tests) == expected["tests_cnt"]
+
+
+def test_tt_torch_full_model_tests_parser_custom_error_message():
+    filepath = f"test/data/tt_torch_models/mnist_custom_error_message.xml"
+    parser = PythonPytestParser()
+    assert parser.can_parse(filepath)
+    tests = parser.parse(filepath, project="tt-torch", github_job_id=5)
+    assert len(tests) == 3
+    # Check that if a custom error message is provided, it is used in a non-xfail/skip case
+    assert tests[0].error_message is not None
+
+    # Check that if a test is skipped, the error message pulls the pytest.skip message
+    assert "[pytest.skip]" in tests[1].error_message
+
+    # Check that if a test is skipped, the error message pulls the pytest.skip message
+    # instead of the custom error message when both are present
+    assert "[pytest.skip]" in tests[2].error_message
