eeaf79e14c17a1aaf8385284295e87acc0a8b857
Collin Tod <collintod@tenstorrent.com>
2025-09-17 16:41:58 +0000

    Add test for `optests.py`

--- .github/actions/collect_data/src/optests.py
+++ .github/actions/collect_data/src/optests.py
@@ -52,7 +52,7 @@ def create_optest_reports(pipeline, workflow_outputs_dir):
 
     for github_job_id, test_reports in github_job_id_to_test_reports.items():
         tests = []
-        job_name = job_id_to_name.get(github_job_id, "")
+        job_name = str(job_id_to_name.get(github_job_id, ""))
 
         # Select parser based on job name and git branch
         if should_use_builder_pytest_parser(job_name, git_branch):
---
+++ .github/actions/collect_data/test/test_optests.py
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+import pytest
+from unittest.mock import MagicMock
+from typing import Optional
+from optests import should_use_builder_pytest_parser, create_optest_reports
+
+
+@pytest.fixture
+def pipeline():
+    pipeline = MagicMock()
+    pipeline.git_branch_name = "main"
+    pipeline.github_pipeline_id = "17415392798"
+    pipeline.jobs = [MagicMock(github_job_id=49443670279, name="test-reports-builder-n150-tracy-silicon-49443670279")]
+    return pipeline
+
+
+@pytest.fixture()
+def workflow_outputs_dir() -> str:
+    return "test/data/"
+
+
+def test_report_discovery(pipeline, workflow_outputs_dir: str) -> None:
+    reports = create_optest_reports(pipeline, workflow_outputs_dir)
+    assert len(reports) == 1
+
+
+@pytest.mark.parametrize(
+    "job_name,branch,result",
+    [
+        ("builder", "main", True),
+        ("blah_blah_builder_blah_blah", "main", True),
+        ("other_job_name", "main", False),
+        (None, None, False),
+        ("builder", "feature_branch", False),
+        ("other_job_name", "feature_branch", False),
+    ],
+)
+def test_should_use_builder_parser(job_name: Optional[str], branch: Optional[str], result: bool) -> None:
+    assert should_use_builder_pytest_parser(job_name, branch) == result
